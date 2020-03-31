d097870c9c47cbd98330db311267fc8dd6ef7725
Abhinav Gunishety <ag513@student.le.ac.uk>
2020-04-01 00:21:35 +0100

    Added files

---
+++ precache-manifest.55c0165085f4bef99bc1329d9e5c69cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fd810ca24be1c556f2908a8ca11d39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19e2fea1cfb562fa73f7",
+    "url": "/static/css/2.c60feeff.chunk.css"
+  },
+  {
+    "revision": "6ad06e2af5fce2a3091b",
+    "url": "/static/css/main.8f00ec1d.chunk.css"
+  },
+  {
+    "revision": "19e2fea1cfb562fa73f7",
+    "url": "/static/js/2.fca97673.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.fca97673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad06e2af5fce2a3091b",
+    "url": "/static/js/main.6e3e2d31.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
